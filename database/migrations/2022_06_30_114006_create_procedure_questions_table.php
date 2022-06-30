4e57bc818a99af8334c055bb72bff78cc24016cd
cooldev225 <goldstar22501@gmail.com>
2022-06-30 22:04:22 +1000

    ProcedureQuestion CRUD added

---
+++ database/migrations/2022_06_30_114006_create_procedure_questions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('procedure_questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('organization_id');
+            $table->string('question');
+            $table->enum('status', ['Enabled', 'Disabled'])->default('Enabled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('procedure_questions');
+    }
+};
