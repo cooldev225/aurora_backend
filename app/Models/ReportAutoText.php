3a5a52dec17cf29fa317121c1bbfd0ee4040e8b9
cooldev225 <goldstar22501@gmail.com>
2022-07-16 17:44:05 +0900

    Report Template Seeding
    - Report Template Factory
    - Report Template Seeder

--- app/Models/ReportAutoText.php
+++ app/Models/ReportAutoText.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ReportAutotext extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['section_id', 'text'];
 
     /**
---
+++ database/factories/ReportTemplateFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Organization;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PatientOrganization>
+ */
+class ReportTemplateFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'organization_id'   => Organization::inRandomOrder()->first()->id,
+            'title'             => $this->faker->sentence(),
+        ];
+    }
+}
