c0230be6aafe20f81e149100c03badc7cc915f96
cooldev225 <goldstar22501@gmail.com>
2022-07-28 15:12:33 +1000

    Generated mailbox and mail model

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mail;
+use Illuminate\Http\Response;
+use App\Http\Requests\MailRequest;
+
+class MailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\MailRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(MailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Mail  $mail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mail $mail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Mail  $mail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mail $mail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\MailRequest  $request
+     * @param  \App\Models\Mail  $mail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(MailRequest $request, Mail $mail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Mail  $mail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mail $mail)
+    {
+        //
+    }
+}
