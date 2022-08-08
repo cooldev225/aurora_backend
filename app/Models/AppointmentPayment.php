263e1c58e4fc5756ac03aa0ac5645b57cd22c474
cooldev225 <goldstar22501@gmail.com>
2022-08-08 16:53:26 +1000

    Parse date on make payment

--- app/Models/AppointmentPayment.php
+++ app/Models/AppointmentPayment.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,4 +20,11 @@ class AppointmentPayment extends Model
     {
         return $this->belongsTo(Appointment::class, 'appointment_id');
     }
+
+  
+
+public function getCreatedAtAttribute($value)
+{
+    return Carbon::parse($value)->format('Y-m-d H:i');
+}
 }
