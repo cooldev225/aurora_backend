e2478a0762cc78cf97a86469a4f71b5f8efa1358
cooldev225 <goldstar22501@gmail.com>
2022-08-29 15:01:40 +1000

    Update documentation error

--- app/Http/Controllers/AppointmentSearchAvailableController.php
+++ app/Http/Controllers/AppointmentSearchAvailableController.php
@@ -18,11 +18,11 @@ class AppointmentSearchAvailableController extends BaseOrganizationController
      * @group Appointments
      * @param  \App\Http\Requests\Request  $request
      * @param  \App\Models\Appointment  $appointment
-     * @clinic_id           A Clinic id.                                Example: 1
-     * @x_weeks             Number of weeks on the future to search.    Example: 2
-     * @specialist_id       A Specialist Id.                            Example: 16
-     * @appointment_type_id An Appointment Type id.                     Example: 3
-     * @time_requirement    A Time Requirement Id.                      Example: 4
+     * @urlParam clinic_id           A Clinic id.                                Example: 1
+     * @urlParam x_weeks             Number of weeks on the future to search.    Example: 2
+     * @urlParam specialist_id       A Specialist Id.                            Example: 16
+     * @urlParam appointment_type_id An Appointment Type id.                     Example: 3
+     * @urlParam time_requirement    A Time Requirement Id.                      Example: 4
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
