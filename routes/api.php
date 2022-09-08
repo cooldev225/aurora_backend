d8f1f3e12afaa1f0672103a3d32073cac4b50c4c
cooldev225 <goldstar22501@gmail.com>
2022-09-08 13:47:17 +1000

    Change route to put

--- routes/api.php
+++ routes/api.php
@@ -113,7 +113,7 @@ Route::middleware(['auth'])->group(function () {
     // Patient Routes
     Route::prefix('patients')->group(function () {
         Route::get('/appointments/{patient}', [PatientController::class, 'appointments']);
-        Route::get('/billing/{patient}', [PatientBilling::class, 'update']);
+        Route::put('/billing/{patient}',      [PatientBilling::class, 'update']);
        
         Route::get('/documents/{patient}',    [PatientDocumentController::class, 'index']);
         Route::post('/documents/{patient}',   [PatientDocumentController::class, 'store']);
