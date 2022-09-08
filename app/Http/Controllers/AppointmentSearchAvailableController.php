654b805caf6b1d4dff829961f61bb246a4cc6d19
cooldev225 <goldstar22501@gmail.com>
2022-09-08 11:57:25 +1000

    Create Appointment Search Method

--- app/Http/Controllers/AppointmentSearchAvailableController.php
+++ app/Http/Controllers/AppointmentSearchAvailableController.php
@@ -8,6 +8,9 @@ use App\Models\Appointment;
 use App\Models\AppointmentType;
 use App\Models\Specialist;
 use App\Models\AppointmentTimeRequirement;
+use App\Models\Clinic;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class AppointmentSearchAvailableController extends Controller
 {
@@ -27,12 +30,114 @@ class AppointmentSearchAvailableController extends Controller
      */
     public function index(Request $request)
     {
+       
+        // Appointment Type
+        $appointmentType = AppointmentType::find($request->appointment_type_id)->first();
+
+        //Clinic Id
+        $clinicId = $request->clinic_id;
+        Log::info($clinicId );
+
+        // Search date date
+        $searchDate =  strtotime('+'.$request->x_weeks .' weeks',strtotime('monday this week'));
+
+        // Time Frame To Search
+        $timeframeParameters = $this->getTimeFrameParameter($request->time_requirement);
+        $timeslotLength = $timeframeParameters['timeslotLength'];
+        $startTime = $timeframeParameters['startTime'];
+        $endTime = $timeframeParameters['endTime'];
+       
+
+
         // Return a week long list of available appointment slots within the given parameters
+        $days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+        $availableStartTimes = [];
+        foreach ($days as $day) {
+            $availableTimeslots = [];
+
+            // Get All specialist working on given day
+            $specialists = User::where('organization_id', auth()->user()->organization_id)
+                ->where('role_id', 5) 
+                ->whereHas('hrmUserBaseSchedules', function ($query) use ($day, $clinicId ) {
+                    $query->where('week_day', $day);
+                    if($clinicId != ""){
+                        $query->where('clinic_id', $clinicId );
+                    }
+                })->get();
+
+
+            $timeslotFilled = false;
+            for ($time = $startTime; $time < $endTime; $time += $timeslotLength * 60) {
+                foreach ($specialists as $specialist) {
+                    
+                    // Check each specialist available in timeslot and that that they can undergo that appointment type
+                    if ($specialist->canWorkAt($time, $day) && $specialist->canAppointmentTypeAt($time, $day, $appointmentType)) {   
+                        // Check if specialist already has an appointment in timeslot
+                        if(!$specialist->hasAppointmentAtTime($time, $searchDate)){
+                            $hrmUserBaseSchedule = $specialist->hrmUserBaseScheduleAtTimeDay($time, $day);
+                            array_push($availableTimeslots, [
+                              'time' =>  date('H:i', $time),
+                              'specialist_name' => $specialist->full_name,
+                              'specialist_id' => $specialist->id,
+                              'clinic_id' => $hrmUserBaseSchedule->clinic_id,
+                              'clinic_name' => Clinic::find($hrmUserBaseSchedule->clinic_id)->name,
+                            ]);
+                            $timeslotFilled = true;
+                        } 
+                        if($timeslotFilled){
+                            $timeslotFilled = false;
+                            break;
+                        }
+                       
+                    }
+                    
+                }
+          
+            }
+
+            array_push($availableStartTimes, [
+                'day' => $day,
+                'date' => date('d/m/Y', $searchDate),
+                'available_timeslots' => $availableTimeslots
+            ]);
+
+            $searchDate = strtotime("+1 day", $searchDate);
+        }
+
 
         return response()->json(
-            ['message' => 'METHOD NOT IMPLEMENTED'],
+            [
+                'message' => 'METHOD NOT IMPLEMENTED',
+                'data' => $availableStartTimes
+            ],
             Response::HTTP_OK
         );
     }
 
+    private function getTimeFrameParameter($time_requirement)
+    {
+
+        $organization = auth()->user()->organization;
+        $timeslotLength = $organization->appointment_length;
+        $startTime = strtotime($organization->start_time);
+        $endTime = strtotime($organization->end_time);
+        if ($time_requirement) {
+            $timeRequirement = AppointmentTimeRequirement::find($time_requirement);
+            $baseTime = strtotime($timeRequirement->base_time);
+            if ($timeRequirement->type == 'After') {
+                if ($startTime < $baseTime) {
+                    $startTime = $baseTime;
+                }
+            } else if ($timeRequirement->type == 'Before') {
+                if ($endTime > $baseTime) {
+                    $endTime = $baseTime;
+                }
+            }
+        }
+        return [
+            'timeslotLength' => $timeslotLength,
+            'startTime'      => $startTime,
+            'endTime'        => $endTime
+        ];
+    }
 }
