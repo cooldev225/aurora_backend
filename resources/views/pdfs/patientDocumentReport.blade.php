fc25b0ef87a4f2d28a3b52484339478ab835cc1c
cooldev225 <goldstar22501@gmail.com>
2022-09-30 13:40:47 +1000

    Patch: adjust patient document layout

--- resources/views/pdfs/patientDocumentReport.blade.php
+++ resources/views/pdfs/patientDocumentReport.blade.php
@@ -1,31 +1,57 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>{{ $title }}</title>
-</head>
-<body>
-    <div>
-        <img src="{{ storage_path('app/'.$header_image) }}" style="width: 100%;">
-    </div>
-    <h1>{{ $title }}</h1>
-    <div>
-        <span>Date</span><p>{{ $date }}</p>
-    </div>
-    <div>
-        <p>Dear {{$referringDoctor}}, Thank you for referring {{$patientName}}</p>
-    </div>
-    @foreach ($reportData as $report)
-        <h2>{{ $report[0]}}</h2>
-        @foreach ($report[2] as $answer)
-          <p>
-            {{$answer}}
-          </p>
-        @endforeach
-        <h3>{{ $report[1]}}</h3>
+    <style>
+        html,
+        body {
+            margin: 0px;
+            height: 100%;
+        }
 
-    @endforeach
-    <div>
-        <img src="{{ storage_path('app/'.$footer_image) }}" style="width: 100%;">
-    </div>
+        footer {
+            position: absolute;
+            bottom: 0px;
+        }
+
+        section {
+            padding: 20px 40px 30px 40px;
+        }
+
+        .light {
+            color: gray;
+            font-size: small;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <img src="{{ storage_path('app/' . $header_image) }}" style="width: 100%;">
+    </header>
+    <section>
+        <h1>{{ $title }}</h1>
+        <div>
+            <p>Dear {{ $referringDoctor }}, Thank you for referring {{ $patientName }}</p>
+        </div>
+        @foreach ($reportData as $report)
+            <h2>{{ $report[0] }}</h2>
+            @foreach ($report[2] as $answer)
+                <p>
+                    {{ $answer }}
+                </p>
+            @endforeach
+            <h3>{{ $report[1] }}</h3>
+        @endforeach
+        <div>
+
+            <p>Report created by name at  {{ $date }}</p>
+        </div>
+    </section>
+    <footer>
+        <img src="{{ storage_path('app/' . $footer_image) }}" style="width: 100%;">
+    </footer>
 </body>
+
 </html>
