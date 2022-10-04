83b4ebcf7c74f397b7358cebb1e228a0fb1f4038
cooldev225 <goldstar22501@gmail.com>
2022-10-05 08:11:38 +1100

    Add is_complete field to codes

--- app/Models/AppointmentCodes.php
+++ app/Models/AppointmentCodes.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class AppointmentCodes extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['diagnosis_codes','indication_codes','extra_items_used','is_complete','appointment_id','procedures_undertaken'];
 }
--- database/migrations/2022_10_04_141601_create_appointment_codes_table.php
+++ database/migrations/2022_10_04_141601_create_appointment_codes_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('appointment_codes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('appointment_id');
             $table->text('procedures_undertaken');
             $table->text('extra_items_used'); 
             $table->text('indication_codes');
             $table->text('diagnosis_codes');
+            $table->boolean('is_complete');
             $table->timestamps();
         });
     }
