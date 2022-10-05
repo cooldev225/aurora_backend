f6716402fb6c2f8bbd75c77f4d7dea9394bb7408
cooldev225 <goldstar22501@gmail.com>
2022-10-05 12:47:27 +1100

    Add Document to coding index

--- app/Http/Controllers/CodingController.php
+++ app/Http/Controllers/CodingController.php
@@ -18,8 +18,36 @@ class CodingController extends Controller
         // Verify the user can access this function via policy
         $this->authorize('viewAny', Appointment::class);
 
+        $appointments = Appointment::where('organization_id', auth()->user()->organization_id)
+            ->with('codes')
+            ->with([
+                'documents' => function ($query) {
+                    $query->where('document_type', 'REPORT');
+                }
+            ])
+        ->get();
+
+        return response()->json(
+            [
+                'message' => 'Admin List',
+                'data' => $appointments,
+            ],
+            Response::HTTP_OK
+        );
+    }
+
+   /**
+     * [Admin User] - List
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        // Verify the user can access this function via policy
+        $this->authorize('viewAny', Appointment::class);
+
         $appointments = Appointment::where('organization_id', auth()->user()->organization_id)->with('codes')->get();
-// ADD a where has report
+
         return response()->json(
             [
                 'message' => 'Admin List',
