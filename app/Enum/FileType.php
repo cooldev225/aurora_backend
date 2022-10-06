10ca5a2ad8d9262255846d5e91d770cb956561d2
cooldev225 <goldstar22501@gmail.com>
2022-10-06 16:31:39 +0900

    work-05: complete image preview in list

--- app/Enum/FileType.php
+++ app/Enum/FileType.php
@@ -7,9 +7,12 @@ enum FileType: string
     case REFERRAL = 'referral';
     case PRE_ADMISSION = 'pre_admission';
     case PATIENT_DOCUMENT = 'patient_document';
+    case ORGANIZATION_HEADER = 'header__organization';
+    case ORGANIZATION_FOOTER = 'footer__organization';
     case ORGANIZATION_LOGO = 'logo_organization';
     case USER_PHOTO = 'user_photo';
     case DOCUMENT_HEADER = 'document_header';
     case DOCUMENT_FOOTER = 'document_footer';
-
+    case CLINIC_HEADER = 'clinic_header';
+    case CLINIC_FOOTER = 'clinic_footer';
 }
--- app/Models/DocumentHeaderFooterTemplate.php
+++ app/Models/DocumentHeaderFooterTemplate.php
@@ -4,8 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
 
 class DocumentHeaderFooterTemplate extends Model
 {
@@ -15,39 +13,11 @@ class DocumentHeaderFooterTemplate extends Model
         'organization_id', 'title', 'header_file', 'footer_file', 'is_organization_default'
     ];
 
-    protected $appends = array('header_file_src', 'footer_file_src');
-
     /**
      * Return Organization
      */
     public function organization()
     {
         return $this->belongsTo(Organization::class);
-    }
-
-    /*
-    * Get header file content
-    */
-    public function getHeaderFileSrcAttribute()
-    {
-        $folder = getUserOrganizationFilePath('files');
-
-        $path = "{$folder}/{$this->header_file}";
-        $file = Storage::disk('local')->get($path);
-        Log::info($path);
-        return $path;//base64_decode($file);
-    }
-
-    /*
-    * Get footer file content
-    */
-    public function getFooterFileSrcAttribute()
-    {
-        $folder = getUserOrganizationFilePath('files');
-
-        $path = "{$folder}/{$this->footer_file}";
-        $file = Storage::disk('local')->get($path);
-        Log::info($path);
-        return $path;//base64_decode($file);
     }
 }
