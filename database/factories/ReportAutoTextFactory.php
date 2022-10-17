18ac9276a2be13065ff81a7450d2360a6d6b1b80
cooldev225 <goldstar22501@gmail.com>
2022-10-18 10:59:37 +1100

    Update ReportAutoTextFactory.php

--- database/factories/ReportAutoTextFactory.php
+++ database/factories/ReportAutoTextFactory.php
@@ -19,7 +19,14 @@ class ReportAutoTextFactory extends Factory
     {
         return [
             'section_id'    => ReportSection::inRandomOrder()->first()->id,
-            'text'          => $this->faker->paragraph(),
+            'text'          => $this->faker->sentence(rand(3,5)),
+            'icd_10_code'   =>$this->faker->randomElement([
+                'A04.71 - Enterocolitis due to Colostridium',
+                'H70.00 - Acute mastoiditis without complications',
+                'H70.02 - Petrositis',
+                'H70.08 - Other mastoiditis and related conditions',
+                'H70.09 - Unspecified mastoiditis',
+                'P08.00 - Disorders of newborn related to long gestation and high birth weight']),
         ];
     }
 }
