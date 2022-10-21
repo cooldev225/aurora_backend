7bc2fb7e98f43569088ca52f6580aeaee464a979
cooldev225 <goldstar22501@gmail.com>
2022-10-21 16:46:27 +1030

    Feat: Implement Model and controllers for HRM weekly Schedule and Add seeders

--- routes/api.php
+++ routes/api.php
@@ -173,6 +173,11 @@ Route::middleware(['auth'])->group(function () {
       
     });
 
+    Route::prefix('hrm')->group(function () {
+        Route::apiResource('/weekly-timeslot', HrmWeeklyScheduleController::class);
+//        Route::get('/anesthetists', [AnesthetistController::class, 'index']);
+    });
+
     ////////////////////////////////////////////////////////////////////////////////////
     // Payment Routes
     Route::prefix('payments')->group(function () {
