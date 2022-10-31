e0344d7e195ccd35c4c8a895a9da03645967cea8
cooldev225 <goldstar22501@gmail.com>
2022-10-31 10:58:09 +1030

    Patch: Implement HRM weekly schedule update slots and create slots.

--- app/Http/Controllers/HrmWeeklyScheduleController.php
+++ app/Http/Controllers/HrmWeeklyScheduleController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\HrmWeeklyscheduleIndexRequest;
 use App\Http\Requests\HrmWeeklyScheduleRequest;
 use App\Models\HrmWeeklySchedule;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -16,28 +17,28 @@ class HrmWeeklyScheduleController extends Controller
     public function index(HrmWeeklyscheduleIndexRequest $request)
     {
         $params = $request->validated();
+
         // Verify the user can access this function via policy
         $this->authorize('viewAny', [User::class, auth()->user()->organization_id]);
+
         $organization = auth()->user()->organization;
-        $startDate = "sdfsdf";
-        $endDate = "d";
+        $startDate = Carbon::parse($params['date'])->startOfWeek()->format('Y-m-d');
+        $endDate = Carbon::parse($params['date'])->endOfWeek()->format('Y-m-d');
+
         $users = User::where(
             'organization_id',
             $organization->id
         )->wherenot('role_id', UserRole::ADMIN)
             ->wherenot('role_id', UserRole::ORGANIZATION_ADMIN)
-            ->with(['hrmWeeklySchedule', function ($query) use ($startDate, $endDate) {
-                $query->whereBetween('date', [$startDate, $endDate]);
-            }
-            ])->groupBy('user_id');
-
+            ->with(
+                ['hrmWeeklySchedule' => function ($query) use ($startDate, $endDate) {
+                    $query->whereBetween('date', [$startDate, $endDate]);
+                }
+                ])->get();
         return response()->json(
             [
-                'message' => 'Schedule template',
-                'data' => auth()->user()->organization
-                    ->scheduleTimeslots
-                    ->where('is_template', 1)
-                    ->groupBy('user_id'),
+                'message' => 'Schedule template ' . $startDate . ' ' . $endDate,
+                'data' => $users,
             ],
             200
         );
@@ -88,6 +89,7 @@ class HrmWeeklyScheduleController extends Controller
                     'organization_id' => auth()->user()->organization_id,
                     'start_time' => $slot['start_time'],
                     'end_time' => $slot['end_time'],
+                    'date' => $slot['date'],
                     'clinic_id' => $slot['clinic_id'],
                     'week_day' => $slot['week_day'],
                     'category' => $slot['category'],
@@ -95,6 +97,7 @@ class HrmWeeklyScheduleController extends Controller
                     'user_id' => $slot['user_id'],
                     'is_template' => $slot['is_template'],
                     'anesthetist_id' => $slot['anesthetist_id'],
+                    'status' => $slot['status'],
                 ]);
             }
         }
