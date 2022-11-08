59b7c5bc77ffab3c0608fd12809d6d4923da4a4e
cooldev225 <goldstar22501@gmail.com>
2022-11-08 12:57:57 +0900

    bl-30/work-01: updated scheduleFee controller for create action

--- app/Http/Controllers/ScheduleFeeController.php
+++ app/Http/Controllers/ScheduleFeeController.php
@@ -42,11 +42,21 @@ class ScheduleFeeController extends Controller
     {
         // Verify the user can access this function via policy
         $this->authorize('create', ScheduleFee::class);
-
-        $scheduleFee = ScheduleFee::create([
-            ...$request->validated(),
-            'organization_id' => auth()->user()->organization_id,
-        ]);
+        $scheduleFee = null;
+        $organization_id = auth()->user()->organization_id;
+        $schedule_fees = ScheduleFee::where('organization_id', $organization_id)
+                        ->where("mbs_item_code", $request->mbs_item_code)
+                        ->where("health_fund_code", $request->health_fund_code);
+        if($schedule_fees->count()){
+            $scheduleFee = $schedule_fees->first();
+            $scheduleFee = $scheduleFee->update($request->validated());
+        }else{
+            
+            $scheduleFee = ScheduleFee::create([
+                ...$request->validated(),
+                'organization_id' => auth()->user()->organization_id,
+            ]);
+        }
 
         return response()->json(
             [
